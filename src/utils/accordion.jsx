baa05fe06e89c99b7a172487839211446ed5fbba
WarriorAshish <110679234+WarriorAshish@users.noreply.github.com>
2024-02-18 13:29:24 +0530

    Add files via upload

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Discover your dream property with us and take advantage of the best interest rates on the market. Our real estate website offers a range of options that combine exceptional properties with favorable and competitive interest rates, making your homeownership journey both affordable and rewarding. Explore now for unbeatable rates on your path to owning the perfect home.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Experience stability in the real estate market with us. Our platform promotes steady prices by providing expert insights, real-time data analysis, and informed investment strategies. Say goodbye to uncertainty as we empower you to make confident property decisions in an environment designed to prevent price fluctuations.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Discover your dream property with us! Our real estate website offers the best prices on the market, ensuring you get unparalleled value for your investment. Explore a diverse range of listings and seize the opportunity to find your perfect home at the most competitive prices available. Your real estate journey starts here, where affordability meets excellence.",
+  },
+];
+export default data;
